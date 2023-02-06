6f265b3a0739870c681600a2e999425350c093f3
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 21:13:22 +0300

    js_16

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 "use strict";
 // задание 1
-let str;
-str = '!!!'
-alert(str);
-
-// задание 2
-let a;
-let b;
-a = 'java';
-b = 'script';
-alert(a + b);
-
-// задание 3
-let c;
-let d;
-c = 'hello';
-d = 'world';
-alert(c + d);
+let str = 'edemasanov';
+alert(str.length);
