5410b17f27e5e1b11b80b2be012322a44bc02301
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 21:15:54 +0300

    js_17

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 "use strict";
-// задание 1
-let str = 'edemasanov';
-alert(str.length);
+"use strict";
+let str = `abc
+def`; 
+alert(str);
+
+let a, b, c,d; 
+
+
+a = 'java'.length;
+b = 'script';
+c = 'hello';
+d = ' world';
+alert(a);
+alert(c+d);
+
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = `aaa${str1}bbb${str2}ccc`;
+alert(txt);
+
+let abc = `f
+g
+k
+`;
