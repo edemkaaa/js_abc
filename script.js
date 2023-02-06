3d75ed16e2721b408afaef5799b8221a4365b0ab
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 21:32:17 +0300

    js_20

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 "use strict";
-// задание 1
-let num = 2004;
-console.log(num);
-
-// задание 2
-let num1 = 5;
-let num2 = 10;
-let num3 = 11;
-
-console.log(num1);
-console.log(num2);
-console.log(num3);
-
-// задание 3
-console.log(edem);
-console.log('edem');
-
-// задание 4
-alert(abc);
+const PI = 3.14;
+let a = 10;
+result = 2 * PI *removeEventListener;
+alert(result);
