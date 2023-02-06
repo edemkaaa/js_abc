c6ef5bcf5acb495c553dffb987e309b4754a80d1
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 21:35:01 +0300

    js_21

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 "use strict";
-const PI = 3.14;
-let a = 10;
-result = 2 * PI *removeEventListener;
-alert(result);
+// задание 1
+let a = '5' + '2';
+alert(a);
+
+// задание 2
+let b = '5' + 2;
+alert(b);
+
+// задание 3
+let c = 5 + '2';
+alert(c);
+
+// задание 4
+let d = 5 + 2;
+alert(d);
+
+// задание 5
+let e = '5' * '2';
+alert(e);
