4f122a8026d97d8981e73b151fe8dae95e632dee
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 21:58:31 +0300

    js_27

--- script.js
+++ script.js
@@ -1,65 +1,18 @@
 "use strict";
-let a, b, c, d, result, str;
 // задание 1
-let test = Boolean(3);
-alert(test);
+let str = 'abcde';
+
+alert(str[0]); // выведет 'a'
+alert(str[2]); // выведет 'c'
+alert(str[4]); // выведет 'e'
+
 // задание 2
-test = Boolean(0);
-alert(test);
+alert(str[4]); // выведет 'e'
+alert(str[3]); // выведет 'd'
+alert(str[2]); // выведет 'c'
+alert(str[1]); // выведет 'b'
+alert(str[0]); // выведет 'a'
+
 // задание 3
-test = Boolean(-1);
-alert(test);
-// задание 4
-test = Boolean(-0);
-alert(test);
-// задание 5
-test = Boolean(+0);
-alert(test);
-// задание 6
-test = Boolean('abc');
-alert(test);
-// задание 7
-test = Boolean('');
-alert(test);
-// задание 8
-test = Boolean('0');
-alert(test);
-// задание 9
-test = Boolean(true);
-alert(test);
-// задание 10
-test = Boolean(false);
-alert(test);
-// задание 11
-test = Boolean('true');
-alert(test);
-// задание 12
-test = Boolean('false');
-alert(test);
-// задание 13
-test = Boolean(null);
-alert(test);
-// задание 14
-test = Boolean('null');
-alert(test);
-// задание 15
-test = Boolean(undefined);
-alert(test);
-// задание 16
-test = Boolean('undefined');
-alert(test);
-// задание 17
-test = Boolean(NaN);
-alert(test);
-// задание 18
-test = Boolean('NaN');
-alert(test);
-// задание 19
-test = Boolean(3 * 'abc');
-alert(test);
-// задание 20
-test = Boolean(Infinity);
-alert(test);
-// задание 21
-test = Boolean(1 / 0);
-alert(test);
+let num = 3;
+alert(str[num]);
