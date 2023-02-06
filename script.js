c88fab8e5eacf204467bd0f0be1bb2d834ac021b
edemkaaa <edemasanov2004@gmail.com>
2023-02-06 21:47:19 +0300

    js_25

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 "use strict";
-// задание 1
-let num = parseInt('5px');
-let num2 = parseInt('6px');
-alert(num + num2); 
+let a, b, c, d, result, str;
 
-// задание 2
-let a = parseInt('5.5px');
-let b = parseInt('6.25px');
-alert(a + b); 
-
-// задание 3
-let c = parseInt('5.5px');
-let d = parseInt('6.25px');
-alert(c + d + 'px'); 
+// Присваиваем переменным значения:
+a = 55;
+b = 51;
+alert(String(a) + String(b));
+a = 63;
+alert(String(a).length);
+a = 332;
+b = 1;
+alert(String(a).length + String(b).length); 
