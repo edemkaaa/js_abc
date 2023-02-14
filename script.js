d5672503fec33dce116036f1b6c3675e8323509f
edemkaaa <edemasanov2004@gmail.com>
2023-02-14 20:40:45 +0300

    30

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-let edem = '12345'; // строка с цифрами
-alert(Number(edem[0]) + Number(edem[1]) + Number(edem[2]) + Number(edem[3]) + Number(edem[4]));
-
-// задание 2
-let test1 = String(12345); // строка
-alert(Number(test1[0]) + Number(test1[1]) + Number(test1[2])+ Number(test1[3])+ Number(test1[4])); // выведет 15
-
-// задание 3
-let test2 = String(12345); // строка
-alert(test2[0] * test2[1] * test2[2] * test2[3] * test2[4]); // выведет 120
-
-// задание 4
-let test3 = String(12345); // строка
-alert(test3[4] * test3[3] * test3[2] * test3[1] * test3[0]); // выведет 120
+let num1 = 47;
+num1 += 7;
+num1 -= 18;
+num1 *= 10;
+num1 /= 15;
+console.log(num1);
