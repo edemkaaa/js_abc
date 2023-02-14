e98173b385aed02b695803f08b813fdb0ddff481
edemkaaa <edemasanov2004@gmail.com>
2023-02-14 20:38:54 +0300

    29

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-"use strict";
-
-// задание 1
-let str = 'edem';
-let last = str.length - 1; // номер последнего символа
-alert(str[last]); // выведет 'm'
+let edem = '12345'; // строка с цифрами
+alert(Number(edem[0]) + Number(edem[1]) + Number(edem[2]) + Number(edem[3]) + Number(edem[4]));
 
 // задание 2
-let a = 'asanov';
-let b = a.length - 2; // номер предпоследнего символа
-alert(a[b]); // выведет 'o'
+let test1 = String(12345); // строка
+alert(Number(test1[0]) + Number(test1[1]) + Number(test1[2])+ Number(test1[3])+ Number(test1[4])); // выведет 15
 
 // задание 3
-a = 'asanov';
-b = a.length  -3; // номер предпоследнего символа
-alert(a[b]); // выведет 'n'
+let test2 = String(12345); // строка
+alert(test2[0] * test2[1] * test2[2] * test2[3] * test2[4]); // выведет 120
+
+// задание 4
+let test3 = String(12345); // строка
+alert(test3[4] * test3[3] * test3[2] * test3[1] * test3[0]); // выведет 120
