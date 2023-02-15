e85798b5c9cdfad5c44af83f62856579050fa075
edemkaaa <edemasanov2004@gmail.com>
2023-02-15 16:30:29 +0300

    js_33

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 "use strict";
 
-let num = 50;
-num++;
-num++;
-num--;
-alert(num);
+alert(`Задание 1: Ваш возраст: ${Number(prompt('Ваш возраст?'))}`);
+let num1 = Number(prompt('Введите первое число'));
+let num2 = Number(prompt('Введите второе число'));
+
+alert(`Задание 2: ${Number(num1) + Number(num2)}`);
+let area = Number(prompt('Введите длину стороны квадрата: '));
+alert(`Задание 3: ${area ** 2}`);
+num1 = Number(prompt('Введите длину прямоугольника'));
+num2 = Number(prompt('Введите ширину прямоугольника'));
+alert(`Задание 4: ${Number(num1) * 2 + Number(num2) * 2}`);
