02eba1265c39d9f00054bd9c36c5e399594b70e4
edemkaaa <edemasanov2004@gmail.com>
2023-02-15 16:39:57 +0300

    js_43

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 "use strict";
 
-let arr_1 = ["a", "b", "c"],
-  arr_2 = [1, 2, 3],
-  arr_3 = [1, 2, 3];
-(arr_1[0] = 1), (arr_1[1] = 2), (arr_1[2] = 3);
-document.write(`Задание 1: ${arr_1}<br>`);
-for (let index = 0; index < arr_2.length; index++) {
-  arr_2[index] += 3;
-}
-document.write(`Задание 2: ${arr_2}<br>`);
-for (let index = 0; index < arr_3.length; index++) {
-  arr_3[index]++;
-}
-document.write(`Задание 3: ${arr_3}`);
+let arr = [];
+
+arr[3] = "a";
+arr[8] = "b";
+document.write(`Длина: ${arr.length}<br>Массив: ${arr}`);
