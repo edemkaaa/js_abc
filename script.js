b032f80632228a0db713bda89e2ad1653835d0f3
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:51:27 +0300

    js_83

--- script.js
+++ script.js
@@ -1,79 +1,8 @@
 "use strict";
 
-
-// 1
-let num1 = -10;
-let num2 = -10;
-
-if (num1 >= 0 || num2 >= 0) {
+let num = 3;
+if (num == 0 || num > 1 && num < 5 ) { // если num равен 0 ИЛИ num больше 1 И num < 5 то сработает
 	console.log('+++');
 } else {
-	console.log('---'); // это
-}
-
-// 2
-let num1 = 0;
-let num2 = 0;
-
-if (num1 >= 0 || num2 >= 0) {
-	console.log('+++'); // теперь это
-} else {
 	console.log('---');
 }
-
-// 3
-let num1 = 0;
-let num2 = 5;
-
-if (num1 >= 0 || num2 >= 0) {
-	console.log('+++'); // опять это
-} else {
-	console.log('---');
-}
-
-// 4
-let num1 = 5;
-let num2 = 5;
-
-if (num1 >= 0 || num2 >= 0) {
-	console.log('+++'); // и снова это
-} else {
-	console.log('---');
-}
-
-// 5
-let num1 = -5;
-let num2 = 15;
-
-if (num1 >= 0 || num2 >= 0) { 
-	console.log('+++'); // правда
-} else {
-	console.log('---');
-}
-
-// 6 
-let num = 1;
-
-if (num == 0 || num == 1) {
-	console.log('+++'); // это
-} else {
-	console.log('---');
-}
-
-// 7 
-let num = 2;
-
-if (num == 0 || num == 1) {
-	console.log('+++');
-} else {
-	console.log('---'); // ура это
-}
-
-// 8
-let num = 2;
-
-if (num == 0 || num == 1 || num == 2) {
-	console.log('+++'); // правда
-} else {
-	console.log('---');
-} 
