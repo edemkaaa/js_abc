75f477540d39c00108f7a8fe3fa6b273842835ba
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:12:01 +0300

    js_65

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 "use strict";
 
-console.log( typeof {x: 1, y: 2, z: 3} ); // выведет object
+      console.log(typeof { x: 1, y: 2, z: 3 }); // выведет object
 
-console.log( typeof {} ); // выведет object
+      console.log(typeof [1, 2, 3]); // выведет object
 
-let obj = {x: 1, y: 2, z: 3};
-console.log( typeof obj ); // выведет object
+      let arr = [1, 2, 3];
+      console.log(typeof arr); // выведет object
 
-let obj1 = {x: 1, y: 2, z: 3};
-console.log( typeof obj1['x'] ); // выведет number
+      let arr1 = [1, 2, 3];
+      console.log(typeof arr1[0]); // выведет number
+
+      let arr2 = ['1', '2', '3'];
+      console.log(typeof arr2[0]); //выведет string
