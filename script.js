9f6706dc98109dcf80f6fb7ec7cf3d6e8a7fbae8
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:13:30 +0300

    js_66

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 "use strict";
 
-      console.log(typeof { x: 1, y: 2, z: 3 }); // выведет object
+console.log( Array.isArray([1, 2, 3]) ); // выведет false
 
-      console.log(typeof [1, 2, 3]); // выведет object
-
-      let arr = [1, 2, 3];
-      console.log(typeof arr); // выведет object
-
-      let arr1 = [1, 2, 3];
-      console.log(typeof arr1[0]); // выведет number
-
-      let arr2 = ['1', '2', '3'];
-      console.log(typeof arr2[0]); //выведет string
+console.log( Array.isArray({x: 1, y: 2, z: 3}) ); // выведет true
