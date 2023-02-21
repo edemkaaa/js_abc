cc54671fb7fa1381224d0fa4b2b8fbdf8f14956b
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:27:09 +0300

    js_71

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 "use strict";
 
-const a = 1;
-const b = 2;
-const c = 3;
-const d = 4;
-const e = 5;
-const v = b + c;
+// 1
+let obj1 = { x: 1, y: 2, z: 3 };
+console.log(obj1.x);
 
-console.log(b);
+// 2
+let obj2 = { x: 1, y: 2, z: 3 };
+let key2 = 'x';
+
+console.log(obj2[key2]);
+
+// 3
+let obj3 = { x: 1, y: 2, z: 3 };
+let sum = obj3['x'] + obj3['y'] + obj3['x'];
+
+console.log(obj3);
+console.log(sum);
+
+// 4
+let obj4 = { x: 1, y: 2, z: 3 };
+console.log(Object.keys(obj4).length);
