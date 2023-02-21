fe556b21ce8c7498dc90c15da95d4f5ddb665dfb
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:17:26 +0300

    js_67

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 "use strict";
+// 1
+let test = {x: 1, y: 2, z: 3};
+console.log(test); // объект
+// 2
+let test = {x: 1, y: 2, z: 3};
+console.log(test.x); // примитив
+// 3
+let test = [1, 2, 3];
+console.log(test); // объект
+// 4
+let test = [1, 2, 3];
+console.log(test[1]); // примитив
+// 5
+let test1 = [1, 2, 3];
+let test2 = 1;
 
-console.log( Array.isArray([1, 2, 3]) ); // выведет false
+console.log(test1); // объект
+// 6 
+let test1 = [1, 2, 3];
+let test2 = 1;
 
-console.log( Array.isArray({x: 1, y: 2, z: 3}) ); // выведет true
+console.log(test1[test2]); // примитив
+
+// string boolean number null
