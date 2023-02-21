63c18d0ed9e2119ee04be6ccfee08fcc03f5e891
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:40:02 +0300

    js_78

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 "use strict";
 
-let test = 'abc';
-let test1 = 'abc1'
-if (test == test1) {
-	console.log('+++'); 
-} else {
-	console.log('---'); // сработает это
-}
+let test1 = '123';
+let test2 = 123;
+
+if (test1 == test2) {
+    console.log('Переменная test1 равна test2')
+ } else {
+    console.log('Переменная test1 не равна test2')
+ }
