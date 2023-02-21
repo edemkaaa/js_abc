322cd570ac4d0ef0b2da62b0b3a6b37ac2467dfa
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:38:57 +0300

    js_77

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 "use strict";
 
-// 1
-let test1 = 2, test2 = 3;
-
-if (test1 < test2) {
-    console.log('Переменная test1 меньше test2')
+let test = 'abc';
+let test1 = 'abc1'
+if (test == test1) {
+	console.log('+++'); 
 } else {
-    console.log('Переменная test1 больше test2')
-}
-
-// 2
-let test3 = 3, test4 = 3;
-
-if (test3 == test4) {
-    console.log('Переменная test1 равна test2')
-} else {
-    console.log('Переменная test1 не равна test2')
+	console.log('---'); // сработает это
 }
