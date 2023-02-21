f57f71c8c6cc4c31bf66b4e4ddf30c03255f0606
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:41:22 +0300

    js_79

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
 "use strict";
 
-let test1 = '123';
-let test2 = 123;
+// 1
+ let test1 = '3';
+ let test2 = '3';
 
-if (test1 == test2) {
-    console.log('Переменная test1 равна test2')
+ if (test1 == test2) {
+    console.log('+++'); // выведет эту строку
  } else {
-    console.log('Переменная test1 не равна test2')
+    console.log('---');
+ }
+
+// 2
+ let test3 = '3';
+ let test4 = '3';
+
+ if (test3 === test4) {
+    console.log('+++'); // выведет эту строку
+ } else {
+    console.log('---');
+ }
+
+// 3
+ let test5 = 3;
+ let test6 = '3';
+
+ if (test5 == test6) {
+    console.log('+++'); // выведет эту строку
+ } else {
+    console.log('---');
+ }
+
+// 4
+ let test7 = 3;
+ let test8 = '3';
+
+ if (test7 === test8) {
+    console.log('+++');
+ } else {
+    console.log('---'); // выведет эту строку
+ }
+
+// 5
+ let test9 = 3;
+ let test10 = 3;
+
+ if (test9 === test10) {
+    console.log('+++'); // выведет эту строку
+ } else {
+    console.log('---');
  }
