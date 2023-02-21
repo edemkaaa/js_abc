8fe54444f460d2c79d11469b5e2aace7a124c1ef
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:06:39 +0300

    js_62

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 "use strict";
 
-// ошибка 1
-let key = 'x';
+let obj = {x: 1, y: 2, z: 3};
 
-let obj = {
-	[key]: 1,
-	y: 2,
-	z: 3
-};
-// задание 2
-let key1 = "x";
-let key2 = "y";
-let key3 = "z";
-
-let obj2 = {
-  x: [key1],
-  y: [key2],
-  z: [key3],
-};
+console.log('x' in obj); // правда
+console.log('w' in obj); // ложь
