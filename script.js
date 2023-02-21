4da7e43afe2be0870bf40a47b5973fde7ebda227
edemkaaa <edemasanov2004@gmail.com>
2023-02-21 21:35:21 +0300

    js_74

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
 "use strict";
 
-// 1
-let test = 1;
+let test = 10;
 
-if (test > 10) {
+if (test == 10) {
 	console.log('+++'); // сработает это
 } else {
-	console.log('---');
-}
-// 2
-let test = 1;
-
-if (test < 10) {
-	console.log('+++'); // сработает это
-} else {
-	console.log('---');
-}
-// 3
-let test = 1;
-
-if (test >= 10) {
-	console.log('+++'); // сработает это
-} else {
-	console.log('---');
-}
-
-// 4
-let test = 1;
-
-if (test <= 10) {
-	console.log('+++'); // сработает это
-} else {
-	console.log('---');
+	console.log('---'); 
 }
